218904c3a3e05c465df7cd0d00f989d217e06b4d
emmanueluwa <emmanuel@fulodev.com>
2022-04-11 16:24:05 +0100

    building and completing the homepage

---
+++ src/components/Home/index.js
@@ -0,0 +1,45 @@
+import LogoTitle from '../../assets/images/logo_f.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useState, useEffect } from 'react';
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  //arrays for animated words
+  const nameArray = ['m', 'm', 'a', 'n', 'u', 'e', 'l']
+  const jobArray = ['f', 'u', 'l', 'l', ' ', 's', 't', 'a', 'c', 'k', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+
+  useEffect(() => {
+      return setTimeout(() => {
+          setLetterClass('text-animate-hover')
+      }, 4000)
+  }, [])
+
+  return (
+      <div className="container home-page">
+          <div className="text-zone">
+              <h1>
+              <span className={letterClass}>H</span>
+              <span className={`${letterClass} _12`}>i,</span>
+              <br /> 
+              <span className={`${letterClass} _13`}>I</span>
+              <span className={`${letterClass} _14`}>'m</span>
+              <img src={LogoTitle} alt="developer" />
+              <AnimatedLetters letterClass={letterClass}
+              strArray={nameArray}
+              index={15}/>
+              <br />
+              <AnimatedLetters letterClass={letterClass}
+              strArray={jobArray}
+              index={22}/>
+              </h1>
+              <h2>Full Stack Developer</h2>
+              <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+          </div>
+
+      </div>
+  );
+}
+
+export default Home
